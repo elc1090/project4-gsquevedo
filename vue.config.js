6f663431776934718032805193c778f8782e9832
Gabriele Quevedo <54038952+gsquevedo@users.noreply.github.com>
2023-07-02 19:09:16 -0300

    pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa : { 
+    name : "Jardim Botânico" , 
+    themeColor : "#ffd100",
+    icons: {
+      favicon32 : 'img/icons/jardim.png' , 
+      favicon16 : 'img/icons/jardim.png' , 
+      appleTouchIcon : 'img/icons/jardim.png' , 
+      maskIcon : 'img/icons/jardim.png' , 
+      msTileImage :'img/icons/jardim.png',
+    }
+  } 
 })
